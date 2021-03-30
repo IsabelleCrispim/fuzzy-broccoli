2a6937d62b547a7a0c89895d688e6850a0456a5e
IsabelleCrispim <49999727+IsabelleCrispim@users.noreply.github.com>
2021-03-29 19:00:38 -0500

    complete quote

--- script.js
+++ script.js
@@ -1,16 +1,63 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+//Show Loader
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true; 
+}
+// Hide Loading 
+function complete () {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+
+    }
+}
 //Get Quote From API
 async function getQuote () {
+    loading();
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
-        const response= await fetch(proxyUrl + apiUrl);
-        const data= await response.json();
-        console.log(data);
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        // if  Author is blank, add 'Unknow'
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // Reduce font size for long quotes
+        if (data.quoteText.lenght > 50) {
+            quoteText.classList.add ('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+        // Stop Loader, Show Quote 
+        complete();
+
     }catch (error) {
+        // getQuote();
         console.log('whoops, no quote',error);
     }
-    
+    // tweer Quote 
+function tweetQuote () {
+    const quote = quoteText.innerText;
+    const author= authorText.innerText;
+    const twittweUrl = `https://twitter.com/intent/tweet?text=${quote} -${author}`;
+    window.open(twittweUrl,'_blank');
+}
+// Event Listeners 
+newQuoteBtn.addEventListener ('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);   
 }
 
-// ON Load 
+// ON Load
+loading(); 
 getQuote();
